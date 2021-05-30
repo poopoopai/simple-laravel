0418f2b96e468952e54a0392e4ada539d551bdbd
poopoopai <sword61040767@gmail.com>
2021-05-30 15:50:10 +0800

    新增 simple-laravel

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        $role = Role::create(['name' => 'writer']);
+        $permission = Permission::create(['name' => 'edit articles']);
+        
+        $role->givePermissionTo($permission);
+        $permission->assignRole($role);
+
+        return 'Success Create Role and Permission';
+    }
+
+    public function showRole()
+    {
+        return Role::all();
+    }
+
+    public function showPermission()
+    {
+        return Permission::all();
+    }
+
+    public function storeUerPermission()
+    {
+        return view('home');
+    }
+
+    public function Test213()
+    {
+        
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\TestController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Auth::routes();
+Route::get('test', [TestController::class, 'index']);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/storeUerPermission', [TestController::class, 'storeUerPermission']);
+Route::get('test2', [TestController::class, 'Test213']);
